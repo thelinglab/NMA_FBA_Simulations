6270847c597a8fb48704e3b84b1d86f44e676f6d
Joshua Foster <joshua.james.foster@gmail.com>
2022-02-22 14:53:49 -0500

    Update createFigures.m

--- createFigures.m
+++ createFigures.m
@@ -1,28 +1,4 @@
 % Creates all figures for the paper
-
-% clear all
-% close all
-
-quickcheck = 1;
-if quickcheck == 1
-  numContrasts = 9;
-else
-  numContrasts = 25;
-end
-if quickcheck == 1
-  numOrientations = 9;
-else
-  numOrientations = 25;
-end
-
-tic
-Figure2A
-Figure2B              
-Figure3C
-Figure3F
-Figure4C
-Figure4E
-Figure5C
-Figure6C
-Figure7C
-toc
+Figure7a
+Figure7b
+Figure7c
